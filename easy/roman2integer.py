0586ddfcd57d42536b24ab47b9da242f9086519c
ezcaoyu <yu.y.cao@ericsson.com>
2018-12-10 09:02:35 +0800

    Initial commit

---
+++ easy/roman2integer.py
@@ -0,0 +1,19 @@
+def execute(roman):
+    roman_dict_common = {
+        "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
+    }
+
+    roman_dict_unique = {
+        "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900
+    }
+    result = 0
+    for key, value in roman_dict_unique.items():
+        result += roman.count(key) * value
+        roman = roman.replace(key, "")
+    for key, value in roman_dict_common.items():
+        result += roman.count(key) * value
+    return result
+
+
+if __name__ == '__main__':
+    print(execute("MCMXCIX"))
